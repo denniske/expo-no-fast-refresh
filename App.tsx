ff652f9ccbbba3d7196d1d5c3f3e41e58bfda610
Dennis Keil <dennis.keil10@gmail.com>
2020-06-10 18:46:21 +0200

    Disable fast refresh

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeModules } from 'react-native';
+
+if (__DEV__) {
+  const { DevSettings } = NativeModules;
+  DevSettings.setHotLoadingEnabled(false);
+  DevSettings.setLiveReloadEnabled(false);
+}
 
 export default function App() {
   return (
